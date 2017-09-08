16569599e79320fe317c9b640156b1eb04e28fe6
Jan-Hendrik Willms <tleilax+github@gmail.com>
2017-09-08 16:20:09 +0200

    adjust translations, fixes #4

--- StudiengangsNewsWidget.class.php
+++ StudiengangsNewsWidget.class.php
@@ -5,15 +5,18 @@
  * @author  Chris Schierholz <chris.schierholz1@uni-oldenburg.de>
  * @version 1.0
  */
-class StudiengangsNewsWidget extends UOL\Plugin implements PortalPlugin
+class StudiengangsNewsWidget extends StudIPPlugin implements PortalPlugin
 {
-    const GETTEXT_DOMAIN = 'studiengangnews-widget';
+    const GETTEXT_DOMAIN = 'studiengang-news-widget';
     /**
      * StudiengangsNewsWidget constructor.
      */
     public function __construct()
     {
         parent::__construct();
+
+        bindtextdomain(static::GETTEXT_DOMAIN, $this->getPluginPath() . '/locale');
+        bind_textdomain_codeset(static::GETTEXT_DOMAIN, 'ISO-8859-1');
 
         StudipAutoloader::addAutoloadPath($this->getPluginPath() . '/models', 'StudiengangsNews');
         StudipAutoloader::addAutoloadPath($this->getPluginPath() . '/classes', 'StudiengangsNews');
@@ -34,6 +37,62 @@ class StudiengangsNewsWidget extends UOL\Plugin implements PortalPlugin
         $this->is_admin  = !empty($faculties);  // True when the user can post news.
 
         PageLayout::addScript($this->getPluginURL() . '/assets/studiengangsnewswidget.js');
+    }
+
+    /**
+     * Plugin localization for a single string.
+     * This method supports sprintf()-like execution if you pass additional
+     * parameters.
+     *
+     * @param String $string String to translate
+     * @return translated string
+     */
+    public function _($string)
+    {
+        $result = static::GETTEXT_DOMAIN === null
+                ? $string
+                : dcgettext(static::GETTEXT_DOMAIN, $string, LC_MESSAGES);
+        if ($result === $string) {
+            $result = _($string);
+        }
+
+        if (func_num_args() > 1) {
+            $arguments = array_slice(func_get_args(), 1);
+            $result = vsprintf($result, $arguments);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Plugin localization for plural strings.
+     * This method supports sprintf()-like execution if you pass additional
+     * parameters.
+     *
+     * @param String $string0 String to translate (singular)
+     * @param String $string1 String to translate (plural)
+     * @param mixed  $n       Quantity factor (may be an array or array-like)
+     * @return translated string
+     */
+    public function _n($string0, $string1, $n)
+    {
+        if (is_array($n)) {
+            $n = count($n);
+        }
+
+        $result = static::GETTEXT_DOMAIN === null
+                ? $string0
+                : dngettext(static::GETTEXT_DOMAIN, $string0, $string1, $n);
+        if ($result === $string0 || $result === $string1) {
+            $result = ngettext($string0, $string1, $n);
+        }
+
+        if (func_num_args() > 3) {
+            $arguments = array_slice(func_get_args(), 3);
+            $result = vsprintf($result, $arguments);
+        }
+
+        return $result;
     }
 
     /**
